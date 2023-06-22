ce6dfe2bf5eb4f58faccd53ae0fd6ce0f780a545
Muhamad Asrori <muhammad.asrori112@gmail.com>
2023-06-22 23:15:57 +0700

    Koneksi to API not fix

--- application/controllers/Produk.php
+++ application/controllers/Produk.php
@@ -47,4 +47,56 @@ class Produk extends CI_Controller
         $this->m_produk->Update($data);
         redirect('welcome/index');
     }
+
+    public function koneksiAPI()
+    {
+        date_default_timezone_set('Asia/Singapore');
+        $url = "https://recruitment.fastprint.co.id/tes/api_tes_programmer";
+        $username = "tesprogrammer" . date('d') . date('m') . date('y') . "C" . date('H');
+        $password = "bisacoding-" . date('d') . "-" . date('m') . "-" . date('y');
+
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query(
+            array(
+                'username' => 'tesprogrammer230623C00', 'password' => '43d85a4391fa66039cabc5c65ee715b8',
+            )
+        ));
+
+
+        $response = curl_exec($ch);
+        $dataJSON = json_decode($response);
+        var_dump($dataJSON);
+        curl_close($ch);
+        try {
+            if ($dataJSON == null) {
+                throw new Exception("Server Down");
+            }
+            $data = $dataJSON->data;
+            foreach ($data as $item) {
+                $no = $item->no; // Mengambil nilai dari properti 'no'
+                $id_produk = $item->id_produk; // Mengambil nilai dari properti 'id_produk'
+                $nama_produk = $item->nama_produk; // Mengambil nilai dari properti 'nama_produk'
+                $kategori = $item->kategori; // Mengambil nilai dari properti 'kategori'
+                $harga = $item->harga; // Mengambil nilai dari properti 'harga'
+                $status = $item->status; // Mengambil nilai dari properti 'status'
+
+                // Menampilkan nilai-nilai yang telah diambil
+                // echo "No: " . $no . "<br>";
+                // echo "ID Produk: " . $id_produk . "<br>";
+                // echo "Nama Produk: " . $nama_produk . "<br>";
+                // echo "Kategori: " . $kategori . "<br>";
+                // echo "Harga: " . $harga . "<br>";
+                // echo "Status: " . $status . "<br>";
+            }
+        } catch (Exception $e) {
+            echo 'Pesan : ' . $e->getMessage();
+            die;
+        }
+
+
+        return $data;
+    }
 }
--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -1,12 +1,3 @@
-<div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom box-shadow">
-	<h5 class="my-0 mr-md-auto font-weight-normal">Test Programmer</h5>
-	<nav class="my-2 my-md-0 mr-md-3">
-		<a class="p-2 text-dark" href="#">Cek Koneksi</a>
-		<a class="p-2 text-dark" href="#">Username</a>
-		<a class="p-2 text-dark" href="#">Password</a>
-	</nav>
-</div>
-
 <div class="pricing-header px-3 py-3 pt-md-5 pb-md-4 mx-auto text-center">
 	<h1 class="display-4">Tabel Produk</h1>
 </div>
