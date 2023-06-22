68f8caa1a9ee97814cddf1b5403fc9da9deb1d11
Muhamad Asrori <muhammad.asrori112@gmail.com>
2023-06-22 13:58:51 +0700

    Replace Base_Url

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/testprogrammer/';
+$config['base_url'] = 'http://localhost/Test_Programmer_Codeigniter3/';
 
 /*
 |--------------------------------------------------------------------------
